2ec6cf8a1ebc051b3f394666820cc7b9270ed85e
Larry Legend <larry@houseoflegend.com>
2012-08-05 21:49:47 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AkbarTracker::Application.config.secret_token = '3b166bd715799377c5cfaccbab417a86228a2bd13b137611390d1fb6da3188c557c01b07e597a8f9a04f2f9750af9329f8657e16f2dbae9b248aec7250b9da2f'
